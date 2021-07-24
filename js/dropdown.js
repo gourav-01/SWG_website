cf3970124f75aa7972c33bee9ea0d63564d1f563
parth nanda <parthnanda002@gmail.com>
2021-07-24 19:45:24 +0530

    added study material

---
+++ js/dropdown.js
@@ -0,0 +1,32 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("chemistry_semester").classList.toggle("show");
+  }
+
+function myFunction2() {
+    document.getElementById("physics_semester").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  function maths1() {
+    window.open("");
+  }
+
+  function maths2() {
+    window.open("");
+  }
